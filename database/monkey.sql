06971b3ffdc84b1fb69f569789be6912359d0b25
apple <wow@github.com>
2019-03-24 18:45:13 +0800

    basic entity

--- database/monkey.sql
+++ database/monkey.sql
@@ -1,7 +1,8 @@
+drop table if exists monkey;
 create table monkey(
-  id int(10) primary key ,
-  name varchar(20) not null
+  id int(10) primary key auto_increment,
+  name varchar(20) not null unique ,
+  birthday DATETIME
 );
 
 desc monkey;
-drop table monkey;
--- src/main/java/com/github/fish56/springdatajpa/entity/Monkey.java
+++ src/main/java/com/github/fish56/springdatajpa/entity/Monkey.java
@@ -1,11 +1,21 @@
 package com.github.fish56.springdatajpa.entity;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name = "monkey")
 public class Monkey {
     @Id
+    @Column(columnDefinition = "int(10)")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false, length = 20)
     private String name;
+
+    @Column(name = "birth_day")
+    @Temporal(TemporalType.DATE)
+    private Date birthDay;
 }
+
