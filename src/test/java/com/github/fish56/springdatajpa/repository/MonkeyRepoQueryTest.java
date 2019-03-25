b963717c6622c509ae1d9c460fceb05e455bc265
apple <wow@github.com>
2019-03-25 08:34:07 +0800

    query with jpa

---
+++ src/test/java/com/github/fish56/springdatajpa/repository/MonkeyRepoQueryTest.java
@@ -0,0 +1,26 @@
+package com.github.fish56.springdatajpa.repository;
+
+import com.github.fish56.springdatajpa.entity.Monkey;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MonkeyRepoQueryTest {
+    @Autowired
+    private MonkeyRepo monkeyRepo;
+
+    @Test
+    public void find(){
+        List<Monkey> monkeys = monkeyRepo.findMonkeyByIdGreaterThan(1l);
+
+        for(Monkey monkey : monkeys){
+            System.out.println(monkey.getId() + ": " + monkey.getName());
+        }
+    }
+}
