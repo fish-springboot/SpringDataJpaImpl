2d34d62945e8f3a7e4538ea88b951966c6233478
apple <wow@github.com>
2019-03-25 08:34:07 +0800

    query with method name of a repository interface

--- src/main/java/com/github/fish56/springdatajpa/repository/MonkeyRepo.java
+++ src/main/java/com/github/fish56/springdatajpa/repository/MonkeyRepo.java
@@ -3,5 +3,8 @@ package com.github.fish56.springdatajpa.repository;
 import com.github.fish56.springdatajpa.entity.Monkey;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MonkeyRepo extends CrudRepository<Monkey, Long> {
+    List<Monkey> findMonkeyByIdGreaterThan(Long id);
 }
